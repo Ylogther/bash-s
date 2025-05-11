234ff55d167ad7cddfc0aec426ad1a539d017238
Ylogther <yvothero@proton.me>
2025-05-10 22:31:31 -0500

    Actualizar instalar_dotfileHypr1.sh

--- instalaciones/instalar_dotfileHypr1.sh
+++ instalaciones/instalar_dotfileHypr1.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "instalando dotfile "
+read -p "¿quieres instalar el dotfile ml4w?(s/n): " respuesta
+
+if [ "$respuesta" == "s" ]; then
+
+echo "instalando dotfile ml4w..."
+
+bash <(curl -s https://raw.githubusercontent.com/mylinuxforwork/dotfiles/main/setup-arch.sh)
+yay -S ml4w-hyprland
+ml4w-hyprland-setup
+
+echo "instalación completada con éxito"
+
+else echo "no se permitió proceder con la instalación cerrando script"
+fi
